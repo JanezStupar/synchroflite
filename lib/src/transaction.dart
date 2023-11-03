4fa2fe356634cfd6f7e5e9ea39a0ecf247195c2f
Janez Stupar <janez@janezstupar.com>
2023-11-03 10:25:56 +0100

    Transaction _rawInsert, _rawUpdate and _rawDelete should not increment HLC.

--- lib/src/transaction.dart
+++ lib/src/transaction.dart
@@ -12,24 +12,24 @@ class TransactionSqfliteCrdt extends TransactionCrdt with SqfliteCrdtImplMixin {
   @override
   Future<R> _rawInsert<T, R>(T db, InsertStatement statement,
       [List<Object?>? args, Hlc? hlc]) {
-    hlc ??= canonicalTime.increment();
     affectedTables.add(statement.table.tableName);
+    hlc ??= canonicalTime;
     return super._rawInsert(db, statement, args, hlc);
   }
 
   @override
   Future<R> _rawUpdate<T, R>(T db, UpdateStatement statement,
       [List<Object?>? args, Hlc? hlc]) {
-    hlc ??= canonicalTime.increment();
     affectedTables.add(statement.table.tableName);
+    hlc ??= canonicalTime;
     return super._rawUpdate(db, statement, args, hlc);
   }
 
   @override
   Future<R> _rawDelete<T, R>(T db, DeleteStatement statement,
       [List<Object?>? args, Hlc? hlc]) {
-    hlc ??= canonicalTime.increment();
     affectedTables.add(statement.table.tableName);
+    hlc ??= canonicalTime;
     return super._rawDelete(db, statement, args, hlc);
   }
 
@@ -39,15 +39,15 @@ class TransactionSqfliteCrdt extends TransactionCrdt with SqfliteCrdtImplMixin {
   }
 
   Future<int> rawUpdate(String sql, [List<Object?>? args]) {
-    return _innerRawUpdate(_txn, sql, args);
+    return _innerRawUpdate(_txn, sql, args, canonicalTime);
   }
 
   Future<int> rawInsert(String sql, [List<Object?>? args]) {
-    return _innerRawInsert(_txn, sql, args);
+    return _innerRawInsert(_txn, sql, args, canonicalTime);
   }
 
   Future<int> rawDelete(String sql, [List<Object?>? args]) {
-    return _innerRawDelete(_txn, sql, args);
+    return _innerRawDelete(_txn, sql, args, canonicalTime);
   }
 
   Batch batch() =>
